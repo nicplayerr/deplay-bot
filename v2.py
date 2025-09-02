f5b6074ac527b7fe8447f3fc24efb80f366ec225
nicplayerr <murilon070@gmail.com>
2025-09-02 18:36:34 -0300

    Update v2.py

--- v2.py
+++ v2.py
@@ -12,7 +12,7 @@ import docker
 import asyncio
 from discord import app_commands
 
-TOKEN = '' # TOKEN HERE
+TOKEN = 'MTQwMzkyMDcwMTQzMzQ0NjQyMA.GblZNQ.2mYrEGu_P3Tma9MXcTLwG5qhTBDLETpzOUcyPs' # TOKEN HERE
 RAM_LIMIT = '2g'
 SERVER_LIMIT = 12
 database_file = 'database.txt'
