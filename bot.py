f16fdbbc8bdd0f33b5e8cdcbdbf9cd889bc9f42e
nicplayerr <murilon070@gmail.com>
2025-09-02 22:50:29 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,8 +47,8 @@ load_dotenv()
 
 # Bot configuration
 TOKEN = os.getenv('MTQxMjYxNDg0ODY1NDczNzQyOA.GToL83.dpzC1dgG-EHqfXkELSv1K2kGM6_P-0SPvEvy9k')
-ADMIN_IDS = {int(id_) for id_ in os.getenv('ADMIN_IDS', '1210291131301101618').split(',') if id_.strip()}
-ADMIN_ROLE_ID = int(os.getenv('ADMIN_ROLE_ID', '1376177459870961694'))
+ADMIN_IDS = {int(id_) for id_ in os.getenv('ADMIN_IDS', '1341911297817710684').split(',') if id_.strip()}
+ADMIN_ROLE_ID = int(os.getenv('ADMIN_ROLE_ID', '1407915916301307944'))
 WATERMARK = "Root Apolinario Serviço VPS"
 WELCOME_MESSAGE = "Seja Bem Vindo! Começe conosco!"
 MAX_VPS_PER_USER = int(os.getenv('MAX_VPS_PER_USER', '3'))
@@ -2655,5 +2655,6 @@ if __name__ == "__main__":
         logger.error(f"Bot crashed: {e}")
 
         traceback.print_exc()
+
 
 
